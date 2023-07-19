eb6a6fdd6a1b1789f9fa8bd97b3700a7f43e0f72
Nic <tianfang07@outlook.com>
2023-07-19 12:17:15 +0800

    detail update

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,7 +4,7 @@ import { useHistory} from 'react-router-dom'
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: blog, isPending, error } = useFetch('https://json-server-for-gojo.vercel.app/blogs' + id);
+  const { data: blog, isPending, error } = useFetch('https://json-server-for-gojo.vercel.app/blogs/' + id);
   const history = useHistory();
 
 
